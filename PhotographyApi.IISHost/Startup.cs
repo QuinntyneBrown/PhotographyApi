3bb9f019ab4f583507f168751b80a234b95b8f22
Quinntyne Brown <qbrown@cetaris.com>
2015-10-22 02:45:02 -0400

    add IISHost configuration

---
+++ PhotographyApi.IISHost/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+using PhotographyApi.API;
+
+[assembly: OwinStartup(typeof(PhotographyApi.IISHost.Startup))]
+
+namespace PhotographyApi.IISHost
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            GlobalConfiguration.Configure(config => PhotographyApi.ApiConfiguration.Install(config, app));
+        }
+    }
+}
---
+++ PhotographyApi/ApiConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Owin;
+
+namespace PhotographyApi
+{
+    public class ApiConfiguration : Common.Web.ApiConfiguration
+    {
+        public new static void Install(HttpConfiguration config, IAppBuilder app)
+        {
+            Common.Web.ApiConfiguration.Install(config, app);
+        }
+    }
+}
